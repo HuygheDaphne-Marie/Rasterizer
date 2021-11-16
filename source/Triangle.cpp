636c6b0e1784914cead658bc6b1aff5abf6d001b
Henri-Thibault Huyghe <henrithibault.huyghe@gmail.com>
2021-11-16 15:20:21 +0100

    Triangle Hit() fix
    sign check was wrong, I think. Reversed the comparator and now seems to work perfectly fine

--- source/Triangle.cpp
+++ source/Triangle.cpp
@@ -11,20 +11,18 @@ Triangle::Triangle(const FPoint3& position, const RGBColor& color, const FPoint3
 bool Triangle::Hit(const FPoint2& pixel) const
 {
 	FVector2 intersectToVertex{ pixel - m_Vertex0.xy };
-
-
 	FVector3 edge{ m_Vertex1 - m_Vertex0 };
-	if (Cross(edge.xy, intersectToVertex) < 0.f) // edgeA
+	if (Cross(edge.xy, intersectToVertex) > 0.f) // edgeA
 		return false;
 
 	intersectToVertex = pixel - m_Vertex1.xy;
 	edge = { m_Vertex2 - m_Vertex1 };
-	if (Cross(edge.xy, intersectToVertex) < 0.f) // edgeB
+	if (Cross(edge.xy, intersectToVertex) > 0.f) // edgeB
 		return false;
 
 	intersectToVertex = pixel - m_Vertex2.xy;
 	edge = { m_Vertex0 - m_Vertex2 };
-	if (Cross(edge.xy, intersectToVertex) < 0.f) // edgeC
+	if (Cross(edge.xy, intersectToVertex) > 0.f) // edgeC
 		return false;
 
 	return true;
